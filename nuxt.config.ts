1ff296729ff453c9d6ebf69cb6ecc4e297180620
afifnandya <afifnandya@gmail.com>
2022-05-27 18:21:56 +0700

    add locale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 import { defineNuxtConfig } from "nuxt";
+import { localeEN, localeTh } from "@afif_hh/web-toolkit/locale";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  buildModules: ["@intlify/nuxt3"],
+  intlify: {
+    vueI18n: {
+      locale: "en",
+      messages: {
+        en: {
+          hello: "Hello",
+          ...localeEN,
+        },
+        th: {
+          hello: "こんにちは",
+          ...localeTh,
+        },
+      },
+    },
+  },
 });
