0c3be357687db7b9b87ef6a63d969c982dde15d3
afifnandya <afifnandya@gmail.com>
2022-11-01 21:31:59 +0700

    refactor

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -3,16 +3,18 @@ import { defineNuxtPlugin } from "#imports";
 import en from "~/locales/en.json";
 import th from "~/locales/th.json";
 
+export const i18nOption = {
+  legacy: false,
+  globalInjection: true,
+  locale: "en",
+  messages: {
+    en,
+    th,
+  },
+};
+
 export default defineNuxtPlugin(({ vueApp }) => {
-  const i18n = createI18n({
-    legacy: false,
-    globalInjection: true,
-    locale: "en",
-    messages: {
-      en,
-      th,
-    },
-  });
+  const i18n = createI18n(i18nOption);
 
   vueApp.use(i18n);
 });
