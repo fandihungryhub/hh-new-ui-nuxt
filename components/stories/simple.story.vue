d7ab7801187fef627cda4faf4f6a05c76963b63d
afifnandya <afifnandya@gmail.com>
2022-11-01 21:35:52 +0700

    create dummy story

---
+++ components/stories/simple.story.vue
@@ -0,0 +1,10 @@
+<template>
+  <Story title="MyStory">
+    <div class="bg-red-500">amks</div>
+    <MyButton />
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import MyButton from "../MyButton.vue";
+</script>
