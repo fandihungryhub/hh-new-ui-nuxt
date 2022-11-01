63d14a9ab01f452bb79c1ee4da7d34dac4aaf2e7
afifnandya <afifnandya@gmail.com>
2022-11-01 21:52:23 +0700

    adjust swiper lib

--- lib/swiper.ts
+++ lib/swiper.ts
@@ -1,16 +1,21 @@
 async function loadSwiper() {
   try {
+    await import("swiper/css");
+    await import("swiper/css/navigation");
+    await import("swiper/css/pagination");
+    await import("~/assets/css/swiper.scss");
     const importSwiper = await import("swiper/vue");
     const importSwiperModule = await import("swiper");
     if (importSwiper && importSwiperModule) {
       const { Swiper, SwiperSlide } = importSwiper;
-      const { Navigation, Pagination } = importSwiperModule;
+      const { Navigation, Pagination, Virtual } = importSwiperModule;
       return {
         main: Swiper,
         slide: SwiperSlide,
         modules: {
           navigation: Navigation,
           pagination: Pagination,
+          virtual: Virtual,
         },
       };
     }
