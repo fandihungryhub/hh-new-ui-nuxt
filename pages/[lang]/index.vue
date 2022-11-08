fbbb5515ef49e30d13ea2a1af195a8a20a5da12c
afifnandya <afifnandya@gmail.com>
2022-11-08 21:06:26 +0700

    feat: home page component

--- pages/[lang]/index.vue
+++ pages/[lang]/index.vue
@@ -1,54 +1,7 @@
 <template>
-  <main class="">
-    <HomeBanner
-      image=""
-      :totalCover="formatThousand(1000)"
-      :cityId="1"
-      client:idle
-    />
-    <div class="hidden" style="height: 900px">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quae minus,
-      mollitia doloribus illum ad harum corrupti laborum quis recusandae atque
-      quo suscipit iure, aspernatur assumenda corporis labore optio eveniet?
-    </div>
-    <div class="container mx-auto">
-      <HomeRestaurantSlider :api-order="3" :homeSectionOrder="1" />
-      <HomeRestaurantSlider :api-order="1" :homeSectionOrder="1" />
-      <HomeRestaurantSlider :api-order="8" :homeSectionOrder="1" />
-      <HomeRestaurantSlider :api-order="6" :homeSectionOrder="1" />
-    </div>
-  </main>
+  <HomePage />
 </template>
 
 <script lang="ts" setup>
-import HomeBanner from "~/partial/home/HomeBanner.vue";
-import "~/assets/css/index.css";
-import HomeRestaurantSlider from "~/partial/home/HomeRestaurantSlider.vue";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "~/assets/css/swiper.scss";
-import "~/partial/home/home.scss";
-import { formatThousand } from "~/helpers/string";
+import HomePage from "~~/partial/home/HomePage.vue";
 </script>
---
+++ partial/home/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="">
+    <HomeBanner
+      image=""
+      :totalCover="formatThousand(1000)"
+      :cityId="1"
+      client:idle
+    />
+    <div class="container mx-auto">
+      <HomeRestaurantSlider :api-order="3" :homeSectionOrder="1" />
+      <HomeRestaurantSlider :api-order="1" :homeSectionOrder="1" />
+      <HomeRestaurantSlider :api-order="8" :homeSectionOrder="1" />
+      <HomeRestaurantSlider :api-order="6" :homeSectionOrder="1" />
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import HomeBanner from "~/partial/home/HomeBanner.vue";
+import "~/assets/css/index.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "~/assets/css/swiper.scss";
+import "~/partial/home/home.scss";
+import { formatThousand } from "~/helpers/string";
+import useConfigStore from "~/stores/config";
+import { onMounted } from "#imports";
+import HomeRestaurantSlider from "~~/partial/home/HomeRestaurantSlider.vue";
+const configStore = useConfigStore();
+await configStore.getConfig();
+onMounted(() => {
+  console.log("index mounted");
+});
+</script>
+
+<script lang="ts">
+export default {
+  name: "HomePage",
+};
+</script>
